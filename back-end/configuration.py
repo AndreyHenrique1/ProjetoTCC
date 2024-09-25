43aa497fa01b81f285176ad500450d9dce5dacd7
MarceloFranMar <marcelo.242121@alunosatc.edu.br>
2024-09-24 21:44:16 -0300

    Correção

--- back-end/configuration.py
+++ back-end/configuration.py
@@ -25,8 +25,7 @@ def configure_routes(app):
     app.register_blueprint(pergunta_route)
     app.register_blueprint(usuario_route)
     app.register_blueprint(blog_route)
-    app.register_blueprint(usuario_route)
-    
+
 def configure_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/projetotcc'
     db.init_app(app)
