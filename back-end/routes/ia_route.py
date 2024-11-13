4e13671c46ef68c4e0a486014ee8d80c3ab89a42
MarceloFM <marcelofranciscomauricio@gmail.com>
2024-11-13 14:08:27 -0300

    Atualização

--- back-end/routes/ia_route.py
+++ back-end/routes/ia_route.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, jsonify
 import openai
 
 ia_route = Blueprint('ia', __name__)
-openai.api_key = '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'
+openai.api_key = 'sk-svcacct-KNCExInW1Ks2qEMqUCVoITq4IA2iPgRkwnVcKMHMp3AvY70tA_uEhdR-S5iYJA8T3BlbkFJIVIfz1Y1LhYl-PRYF8-I-vrqNuB0MLiXShOTisY2s9wHRhkQG4_C2MibeDQE8cAA'
 
 @ia_route.route('/ia', methods=['GET', 'POST'])
 def pagina_ia():
